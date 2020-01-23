1831e73790d03cb772b0a034f85e75ba90fec50a
Chris Martin <chrisabmartin@gmail.com>
2020-01-23 18:58:03 +0100

    setup base file structure

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.module.css';
+
+class App extends Component {
+  render () {
+
+    return (
+      <div>
+        start
+      </div>
+    );
+  }
+}
+
+export default App;
