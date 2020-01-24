89d5b53c276b7162b644bfac915fdd50a81bf178
Chris Martin <chrisabmartin@gmail.com>
2020-01-24 10:50:16 +0100

    dimensions component

--- src/components/Dimensions/Dimensions.js
+++ src/components/Dimensions/Dimensions.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import styles from './Dimensions.module.css';
 
-const dimensions = () => (
-    <div>dimension</div>
-)
+import Button from '../../UI/Button/Button';
+
+const dimensions = () => {
+    const table = [
+        { name: 'Entry height', short: '1,373mm', long: '1,373mm' },
+        { name: 'Ramp length', short: '1,010mm', long: '1,010mm' },
+        { name: 'Ramp width', short: '755mm', long: '755mm' },
+        { name: 'Channel length', short: '1,580mm', long: '1,580mm' },
+        { name: 'External height', short: '1,852mm', long: '1,845mm' },
+        { name: 'External height', short: '4,418mm', long: '4,818mm' }
+     ]
+
+     const renderTableData = () => {
+        return table.map((data, index) => {
+           const { name, short, long } = data;
+           return (
+              <tr key={index}>
+                 <td className={`${styles.tableData} ${styles.tableData___backgroundGrey}`}>{name}</td>
+                 <td className={styles.tableData}>{short}</td>
+                 <td className={styles.tableData}>{long}</td>
+              </tr>
+           )
+        });
+     }
+
+    return (
+        <div>
+            <div class={styles.Dimensions}>
+                <div className={styles.Dimensions_table}>
+                    <div className={styles.Dimnsions_table_heading}>
+                        <span>Short</span>
+                        <span>Long</span>
+                    </div>
+                    <table>
+                        <tbody>
+                            {renderTableData()}
+                        </tbody>
+                    </table>
+                </div>
+                <div className={styles.Dimensions_imageContainerContainer}>
+                    <img className={styles.Dimensions_imageContainerContainer_img} src="https://www.bristolstreetversa.com/custom/49792.jpg"></img>
+                </div>
+            </div>
+            <div className={styles.Dimensions_buttonContainer}>
+                <Button buttonText="View our Versa Connect offers"/>
+            </div>
+        </div>
+    )
+}
 
 export default dimensions;
