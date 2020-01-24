92988eee7b2b1fb16d66c2cdc55b792b9fbfa3b0
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-01-24 17:15:31 +0100

    fix layout image sizing

--- src/components/Layouts/Layouts.js
+++ src/components/Layouts/Layouts.js
@@ -9,11 +9,11 @@ const layouts = () => (
         <div className={styles.Layouts}>
             <div className={styles.Layouts_imageContainer}>
                 <span className={styles.Layouts_imageContainer_text}>Short (based on Ford Tourneo Connect)</span>
-                <img className={styles.Layouts_imageContainer_img} src="https://www.bristolstreetversa.com/custom/50423.jpg"></img>
+                <img className={styles.Layouts_imageContainer_img} src="https://www.bristolstreetversa.com/custom/50422.jpg"></img>
             </div>
             <div className={styles.Layouts_imageContainer}>
                 <span className={styles.Layouts_imageContainer_text}>Long based on Ford Tourneo Grand Connect</span>
-                <img className={styles.Layouts_imageContainer_img} src="https://www.bristolstreetversa.com/custom/50422.jpg"></img>
+                <img className={styles.Layouts_imageContainer_img} src="https://www.bristolstreetversa.com/custom/50423.jpg"></img>
             </div>
             <span>* Wheelchair user can sit a little further forward (not as forward as Row 2 seats)</span>
         </div>
